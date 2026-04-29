a6ebdfbd2cce6a73f8bb4bd5a623f00cae8eaa9a
David Nardoo <your-email@gmail.com>
2026-04-29 13:37:04 +1000

    initial working express app

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// serve EVERYTHING from this folder
+app.use(express.static(__dirname));
+
+// send homepage
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("running on port " + PORT);
+});
