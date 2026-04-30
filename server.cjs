20f937cbe28183ee1e5d73caf940361ee8e38627
nardoophotography-tech <nardoophotography@gmail.com>
2026-04-30 14:36:44 +1000

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -16,15 +16,16 @@ app.use(express.static(path.join(__dirname, "dist")));
 app.get("/pay/:id", (req, res) => {
   const payId = req.params.id;
 
-  if (twilioClient) {
-    twilioClient.messages.create({
-      body: `Payment successful: ${payId}`,
-      from: "+17623713671",
-      to: "+61412345678"
-    })
-    .then(msg => console.log("SMS SENT:", msg.sid))
+  if (twilioClient) try {
+  twilioClient.messages.create({
+    body: `Payment successful: ${payId}`,
+    from: "+17623713671",
+    to: "+61412345678"
+  }).then(msg => console.log("SMS SENT:", msg.sid))
     .catch(err => console.log("SMS FAILED:", err));
-  }
+} catch (err) {
+  console.log("TWILIO CRASH:", err);
+}
 
   res.send(`
     <h1>Payment Successful ✔</h1>
