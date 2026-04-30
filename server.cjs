c41fdf59054de0011423894c1ffa6f307117a8d5
nardoophotography-tech <nardoophotography@gmail.com>
2026-04-30 10:38:19 +1000

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -3,11 +3,11 @@ const path = require("path");
 
 const app = express();
 
-// Serve the built React app
+// IMPORTANT: serve Vite build folder
 app.use(express.static(path.join(__dirname, "dist")));
 
-// Send homepage
-app.get(/.*/, (req, res) => {
+// always send index.html for React routes
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
