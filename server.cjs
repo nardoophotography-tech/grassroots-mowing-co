573127cd5579722714530165af259e786cc1d514
nardoophotography-tech <nardoophotography@gmail.com>
2026-04-30 10:17:18 +1000

    Serve index.html from the dist folder

--- server.cjs
+++ server.cjs
@@ -7,9 +7,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "dist")));
 // send homepage
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
